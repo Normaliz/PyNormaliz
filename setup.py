3b8151b073e2fe3ae7f3344733349c9da75138d9
Sebastian Gutsche <gutsche@momo.math.rwth-aachen.de>
2017-05-07 18:11:10 +0200

    New PyNormaliz version, to be released with Normaliz 3.3.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ else:
 
 setup(
     name = 'PyNormaliz',
-    version = '1.5',
+    version = '1.6',
     description = 'An interface to Normaliz',
     author = 'Sebastian Gutsche, Richard Sieg',
     author_email = 'sebastian.gutsche@gmail.com',
