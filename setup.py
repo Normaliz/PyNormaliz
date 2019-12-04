e062a98b0d1ce148409a1bd233510aa04cd5da03
Max Horn <max@quendi.de>
2019-12-04 09:05:17 +0100

    Simplify the build system
    ... and fix several issues by doing so. In particular, we don't need to
    worry about `configure` working right (which it often didn't, namely if
    the CPPFLAGS/CFLAGS/CXXFLAGS/LDFLAGS didn't align exactly with the
    corresponding Python settings for include_dirs, library_dirs, etc.)

--- setup.py
+++ setup.py
@@ -20,10 +20,7 @@ else:
       "include_dirs": [ normaliz_dir + '/include'],
       "library_dirs": [ normaliz_dir + '/lib'],
       "runtime_library_dirs": [ normaliz_dir + '/lib'],
-      "extra_link_args": ['-Wl,-R' + normaliz_dir + '/lib' ]
     }
-
-libraries = [ 'normaliz', 'gmp', 'flint' ]
 
 class TestCommand(Command):
     user_options = []
@@ -47,35 +44,6 @@ class TestCommand(Command):
         finally:
             os.chdir(old_path)
 
-class build_ext(_build_ext):
-    def run(self):
-        """
-        Run ./configure first and update libraries depending
-        on the generated output.
-        """
-        subprocess.check_call(["make", "configure"])
-        global has_extra_dir
-        my_env = copy(os.environ)
-        if has_extra_dir:
-            my_env.update({ 'CPPFLAGS' : '-I' + normaliz_dir + '/include',
-                            'LDFLAGS' : '-L' + normaliz_dir + '/lib' })
-        try:
-            subprocess.check_call(["sh", "./configure"], env = my_env )
-        except subprocess.CalledProcessError:
-            subprocess.check_call(["cat", "config.log"])
-            raise
-        # configure created config.py that we now import
-        from config import ENFNORMALIZ
-        global libraries
-        if ENFNORMALIZ:
-            print("building with ENFNORMALIZ...")
-            libraries.append("arb")
-            libraries.append("eanticxx")
-        else:
-            print("no ENFNORMALIZ support...")
-
-        _build_ext.run(self)
-
 from os import path
 import io
 this_directory = path.abspath(path.dirname(__file__))
@@ -95,9 +63,9 @@ setup(
     ext_modules = [ Extension( "PyNormaliz_cpp",
                               [ "NormalizModule.cpp" ],
                               extra_compile_args=['-std=c++11'],
-                              libraries=libraries,
+                              libraries=[ 'normaliz' ],
                               **extra_kwds) ],
     
     package_data = {'': [ "COPYING", "GPLv2", "README.md" ] },
-    cmdclass = {'build_ext': build_ext, 'test': TestCommand},
+    cmdclass = {'test': TestCommand},
 )
