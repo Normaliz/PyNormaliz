48c16e6bdf78274a0d8712c00082cc5a8a8f0d14
Winfried Bruns <winfried@ryzen>
2021-01-20 18:09:29 +0100

    Better paths

--- .travis-install-normaliz.sh
+++ .travis-install-normaliz.sh
@@ -11,6 +11,12 @@ cd Normaliz
 
 # install dependencies
 
+if [[ $OSTYPE == darwin* ]]; then ## sets paths etc. for Homebrew LLVM
+    source install_scripts_opt/common.sh
+    [ "$EANTIC" == "yes" ] && ./install_scripts_opt/install_nmz_mpfr.sh && echo "mpfr complete!"
+    [ "$EANTIC" == "yes" ] && ./install_scripts_opt/install_nmz_flint.sh && echo "flint complete!"
+fi
+
 [ "$COCOALIB" == "yes" ] && ./install_scripts_opt/install_nmz_cocoa.sh && echo "cocoalib complete!"
 [ "$FLINT" == "yes" ] && ./install_scripts_opt/install_nmz_flint.sh && echo "flint complete!"
 [ "$EANTIC" == "yes" ] && ./install_scripts_opt/install_nmz_arb.sh && echo "arb complete!"
