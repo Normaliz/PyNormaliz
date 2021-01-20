6ddcd6e833ba022a2fadc63ffa92f0781be5736a
Winfried Bruns <winfried@ryzen>
2021-01-20 18:52:10 +0100

    Now the correct file

--- .ci-install-normaliz.sh
+++ .ci-install-normaliz.sh
@@ -16,6 +16,12 @@ export MAKEFLAGS="-j2"
 
 # install dependencies
 
+if [[ $OSTYPE == darwin* ]]; then ## sets paths etc. for Homebrew LLVM
+    source install_scripts_opt/common.sh
+    [ "$EANTIC" == "yes" ] && ./install_scripts_opt/install_nmz_mpfr.sh && echo "mpfr complete!"
+    [ "$EANTIC" == "yes" ] && ./install_scripts_opt/install_nmz_flint.sh && echo "flint complete!"
+fi
+
 [ "$COCOALIB" == "yes" ] && ./install_scripts_opt/install_nmz_cocoa.sh && echo "cocoalib complete!"
 [ "$FLINT" == "yes" ] && ./install_scripts_opt/install_nmz_flint.sh && echo "flint complete!"
 [ "$EANTIC" == "yes" ] && ./install_scripts_opt/install_nmz_arb.sh && echo "arb complete!"
